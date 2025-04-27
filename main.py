456cb8f62b6f9a0e0677ca001c731ff0675ddd61
oakweb <oakweb@gmail.com>
2025-04-26 23:24:01 -0700

    Remove accidental globals.update call before app instantiation

--- main.py
+++ main.py
@@ -25,7 +25,6 @@ from webhook_route import webhook_bp
 # ──────────────────────────────────────────────────────────────────────────────
 load_dotenv()
 
-app.jinja_env.globals.update(os=os, STATIC_FOLDER=app.static_folder)
 app = Flask(__name__)
 
 # --- make sure our "instance" folder and .db file live next to this main.py ---
